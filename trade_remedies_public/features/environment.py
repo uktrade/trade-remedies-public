d8419c1a2e1bfed25834018966d8631cf02b1923
davecharles <22290851+davecharles@users.noreply.github.com>
2020-11-23 17:48:18 +0000

    feat: add basic behave tests.
    - Updated local settings to include behave essentials
    - Added features folder content including environment and steps

---
+++ trade_remedies_public/features/environment.py
@@ -0,0 +1,40 @@
+"""Behave-Django.
+
+Behave-Django environment module.
+"""
+from django.conf import settings
+
+from behave_django.testcase import BehaviorDrivenTestCase
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+
+def before_scenario(context, scenario):  # no-qa
+    BehaviorDrivenTestCase.host = settings.SELENIUM_HOST
+
+
+def after_feature(context, feature):  # no-qa
+    context.browser.quit()
+
+
+def configure(context):
+    browser = DesiredCapabilities.CHROME
+    if "firefox" == settings.SELENIUM_BROWSER:
+        browser = DesiredCapabilities.FIREFOX
+
+    context.browser = webdriver.Remote(
+        command_executor=f'http://{settings.SELENIUM_HUB_HOST}:4444/wd/hub',
+        desired_capabilities=browser,
+    )
+    context.browser.implicitly_wait(5)
+
+
+def before_feature(context, feature):  # no-qa
+    configure(context)
+
+
+def after_step(context, step):
+    if step.status == 'failed':
+        context.browser.save_screenshot(
+            f'/code/test-reports/bdd-screenshots/{context.scenario.name}-{step.name}.png'
+        )
