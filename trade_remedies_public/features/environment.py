64e080d6bde9dad94fde7ae4915d574db4c48eb7
davecharles <22290851+davecharles@users.noreply.github.com>
2020-11-24 12:09:40 +0000

    fix: ensure screen-shot persisted on scenario failure

--- trade_remedies_public/features/environment.py
+++ trade_remedies_public/features/environment.py
@@ -2,15 +2,21 @@
 
 Behave-Django environment module.
 """
-from django.conf import settings
+import datetime
+import os
 
 from behave_django.testcase import BehaviorDrivenTestCase
+from django.conf import settings
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
+CAPTURE_PATH = '/app/test-reports/bdd-screenshots/'
+
+
 def before_scenario(context, scenario):  # no-qa
     BehaviorDrivenTestCase.host = settings.SELENIUM_HOST
+    context.timestamp = datetime.datetime.now().replace(microsecond=0).isoformat()
 
 
 def after_feature(context, feature):  # no-qa
@@ -27,6 +33,7 @@ def configure(context):
         desired_capabilities=browser,
     )
     context.browser.implicitly_wait(5)
+    make_dir(CAPTURE_PATH)
 
 
 def before_feature(context, feature):  # no-qa
@@ -35,6 +42,16 @@ def before_feature(context, feature):  # no-qa
 
 def after_step(context, step):
     if step.status == 'failed':
-        context.browser.save_screenshot(
-            f'/code/test-reports/bdd-screenshots/{context.scenario.name}-{step.name}.png'
+        assert context.browser.save_screenshot(
+            f'{CAPTURE_PATH}{context.timestamp}-{context.scenario.name}-{step.name}.png'
         )
+
+
+def make_dir(path):
+    """Make directory.
+
+    Checks if directory exists, if not make a directory.
+    :param (str) path: Directory to create.
+    """
+    if not os.path.exists(path):
+        os.makedirs(path)
