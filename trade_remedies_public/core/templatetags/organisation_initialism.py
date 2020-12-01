8f4f16d1472e67fa05cb27e2b73eb8ca0368040e
jwgulley <jon.gulley@oasisllmf.org>
2020-12-01 14:57:14 +0000

    wording update

--- trade_remedies_public/core/templatetags/organisation_initialism.py
+++ trade_remedies_public/core/templatetags/organisation_initialism.py
@@ -2,7 +2,7 @@ from core.templatetags import register
 from django.conf import settings
 
 """
-Template tag to display organisation name and organisation initialism
+Template tag to display organisation initialism
 Usage:
     {% organisation_initialism %}
 """
