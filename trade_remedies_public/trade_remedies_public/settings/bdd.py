5cf5cda92d94b086b88b54c88dc520a984881004
Ross Miller <github@decodedcollective.com>
2020-12-10 16:45:44 +0000

    Add API base URL

--- trade_remedies_public/trade_remedies_public/settings/bdd.py
+++ trade_remedies_public/trade_remedies_public/settings/bdd.py
@@ -1,4 +1,4 @@
 from .local import *  # noqa: F403,  F401
 
-API_TEST_URL = "http://apitest:8000/api-test/"
-
+API_TEST_URL = "http://apitest:8000/api-test-obj/"
+API_BASE_URL = "http://apitest:8000"
