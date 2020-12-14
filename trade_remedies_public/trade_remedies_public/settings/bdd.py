87e01f4c8c39ac70a0d2989502d01e79689e989a
Ross Miller <github@decodedcollective.com>
2020-12-14 13:39:47 +0000

    Tidy up BDD config

--- trade_remedies_public/trade_remedies_public/settings/bdd.py
+++ trade_remedies_public/trade_remedies_public/settings/bdd.py
@@ -1,3 +1,13 @@
 from .local import *  # noqa: F403,  F401
 
+INSTALLED_APPS += [  # noqa
+    "behave_django",
+]
+
 API_BASE_URL = "http://apitest:8000"
+API_URL = f"{API_BASE_URL}/{API_PREFIX}"
+
+# SELENIUM
+SELENIUM_BROWSER = "chrome"
+SELENIUM_HUB_HOST = "selenium-hub"
+SELENIUM_HOST = "public"
