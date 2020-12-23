6d6186679b3db941dbfd24b23678bce2b2f27dd0
Luisella Strona <luisella21@hotmail.com>
2020-12-23 11:49:18 +0000

    Fixed errors in tests.

--- trade_remedies_public/features/shared/login.feature
+++ trade_remedies_public/features/shared/login.feature
@@ -3,14 +3,14 @@ Feature: Login Process
   be possible to log in as an existing user.
 
 
-  Scenario: A user logs into the site
-    When an anonymous user navigates to "initial"
-    And the user supplies correct credentials
-    Then the user dashboard is displayed
-
   Scenario: A user cannot log into the site
     When an anonymous user navigates to "initial"
     And the user supplies wrong credentials
     Then the message "Please correct the following errors" is displayed
+
+  Scenario: A user logs into the site
+    When an anonymous user navigates to "initial"
+    And the user supplies correct credentials
+    Then the user dashboard is displayed
 
 
--- trade_remedies_public/features/steps/login.py
+++ trade_remedies_public/features/steps/login.py
@@ -13,12 +13,15 @@ from trade_remedies_public.constants import (
 
 @when("the user supplies wrong credentials")
 def step_wrong_credentials(context):
+    email = "a@a.com"
+    password = "wrong"
     shared.text_is_visible(context, "Email address")
-    username_input = context.browser.find_element_by_name("email")
-    username_input.send_keys("a@a.com")
-    password_input = context.browser.find_element_by_name("password")
-    password_input.send_keys("wrong")
-    context.browser.find_element_by_name("btn-action").click()
+    email_element = get_element_by_id(context, "email")
+    email_element.send_keys(email)
+    password_element = get_element_by_id(context, "password_id")
+    password_element.send_keys(password)
+    submit_element = get_element_by_id(context, "bdd-submit")
+    submit_element.click()
 
 
 @then('the message "Please correct the following errors" is displayed')
@@ -40,13 +43,6 @@ def enter_account_details(context):
     password_element.send_keys(password)
     submit_element = get_element_by_id(context, "bdd-submit")
     submit_element.click()
-    import time
-
-    time.sleep(5)
-
-
-# wait.until(
-#         lambda driver: driver.current_url == desired_url)
 
 
 @then("the user dashboard is displayed")
