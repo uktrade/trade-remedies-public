f08469508bb826f37f45c2d1ccbbb0af3f2a5042
Dave Charles <22290851+davecharles@users.noreply.github.com>
2021-04-27 15:27:29 +0100

    feat: trlst 296 merge release into develop
    Merge release branch back into develop. Updated
    mismatched deps after conflict resolution required
    when merging release branch with develop. Also
    includes updated version.

--- trade_remedies_public/trade_remedies_public/settings/base.py
+++ trade_remedies_public/trade_remedies_public/settings/base.py
@@ -244,7 +244,6 @@ STATICFILES_DIRS = [
 
 ACCOUNT_INFO_READ_ONLY = os.environ.get("ACCOUNT_INFO_READ_ONLY", "TRUE").upper() == "TRUE"
 
-
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
