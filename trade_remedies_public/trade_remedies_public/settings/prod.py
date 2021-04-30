8c71e3c9b0b5e67574e0203efbed91ddf6b53090
davecharles <22290851+davecharles@users.noreply.github.com>
2021-04-30 15:28:11 +0100

    feat: TRLST-301 add audit log middleware.

--- trade_remedies_public/trade_remedies_public/settings/prod.py
+++ trade_remedies_public/trade_remedies_public/settings/prod.py
@@ -1,3 +1,11 @@
 from .base import *  # noqa
 
 LOGGING = ENVIRONMENT_LOGGING  # noqa: F405
+
+INSTALLED_APPS += [
+    "django_audit_log_middleware",
+]
+
+MIDDLEWARE += [
+    "django_audit_log_middleware.AuditLogMiddleware",
+]
