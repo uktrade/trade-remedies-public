20d387bfa5ccd6cc2ed50d64c763de7ba5bc015f
davecharles <22290851+davecharles@users.noreply.github.com>
2021-05-31 16:40:58 +0100

    feat: add IHTC compliance settings

--- trade_remedies_public/trade_remedies_public/settings/prod.py
+++ trade_remedies_public/trade_remedies_public/settings/prod.py
@@ -9,3 +9,35 @@ INSTALLED_APPS += [  # noqa: F405
 MIDDLEWARE += [  # noqa: F405
     "django_audit_log_middleware.AuditLogMiddleware",
 ]
+
+# ## IHTC compliance
+
+# Set crsf cookie to be secure
+CSRF_COOKIE_SECURE = True
+
+# Set session cookie to be secure
+SESSION_COOKIE_SECURE = True
+
+# Make browser end session when user closes browser
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
+# Set cookie expiry to 4 hours
+SESSION_COOKIE_AGE = 4 * 60 * 60  # 4 hours
+
+# Prevent client side JS from accessing CRSF token
+CSRF_COOKIE_HTTPONLY = True
+
+# Prevent client side JS from accessing session cookie (true by default)
+SESSION_COOKIE_HTTPONLY = True
+
+# Set content to no sniff
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+# Set anti XSS header
+SECURE_BROWSER_XSS_FILTER = True
+
+# Audit log middleware user field
+AUDIT_LOG_USER_FIELD = "username"
+
+# Default value for the X-Frame-Options header used by XFrameOptionsMiddleware.
+X_FRAME_OPTIONS = "DENY"
