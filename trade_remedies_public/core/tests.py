67eba3ff26918caa6c05b666cbd2b32be6ca0271
davecharles <22290851+davecharles@users.noreply.github.com>
2021-06-22 12:25:27 +0100

    fix: escape text_element template tag value to prevent s-xss.

--- trade_remedies_public/core/tests.py
+++ trade_remedies_public/core/tests.py
@@ -1,6 +1,9 @@
-from django.test import TestCase
-from core.utils import split_public_documents
 from django.conf import settings
+from django.template import Template, Context
+from django.test import TestCase
+from django.utils.html import escape
+
+from core.utils import split_public_documents
 
 
 class TestPublicDocumentSplitting(TestCase):
@@ -183,3 +186,15 @@ class TestPublicDocumentSplitting(TestCase):
 
         template_docs, public_docs = split_public_documents(docs)
         self.assertEquals(1, len(template_docs))
+
+
+class TestTextElement(TestCase):
+    def test_value_is_escaped(self):
+        img_tag_str = '<img src="test" />'
+
+        rendered = Template(
+            "{% load text_element %}" "{% text_element id='test' label='Test' value=img_tag_str %}"
+        ).render(Context({"img_tag_str": img_tag_str}))
+
+        assert escape(img_tag_str) in rendered
+        assert rendered.count("src") == 1
