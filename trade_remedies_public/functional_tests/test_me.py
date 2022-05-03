0cf3745f52dd45153cdd2f91c9b5200d414da7b7
Johnny <knowyourlines@outlook.com>
2022-05-03 17:34:40 +0100

    get selenium tests running

---
+++ trade_remedies_public/functional_tests/test_me.py
@@ -0,0 +1,19 @@
+from django.test.testcases import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+class MyTestCase(LiveServerTestCase):
+    def setUp(self):
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        self.browser = webdriver.Chrome('/usr/local/bin/chromedriver', chrome_options=chrome_options)
+
+    def tearDown(self):
+        print('we made it')
+        self.browser.quit()
+
+    def test_smoke(self):
+        self.browser.get('%s%s' % (self.live_server_url, '/'))
