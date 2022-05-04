61a7d84f0428b2e208b3e6adb38e9e88081e3863
Johnny <knowyourlines@outlook.com>
2022-05-04 11:03:20 +0100

    do not specify chromedriver path

--- trade_remedies_public/functional_tests/test_me.py
+++ trade_remedies_public/functional_tests/test_me.py
@@ -9,9 +9,7 @@ class MyTestCase(LiveServerTestCase):
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
-        self.browser = webdriver.Chrome(
-            "/usr/local/bin/chromedriver", chrome_options=chrome_options
-        )
+        self.browser = webdriver.Chrome(chrome_options=chrome_options)
 
     def tearDown(self):
         print("we made it")
