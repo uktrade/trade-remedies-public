0f006298c22428e6ac8d01b64fe3e07a304f6e1c
Johnny <knowyourlines@outlook.com>
2022-05-04 10:42:11 +0100

    black

--- trade_remedies_public/functional_tests/test_me.py
+++ trade_remedies_public/functional_tests/test_me.py
@@ -6,14 +6,16 @@ from selenium.webdriver.chrome.options import Options
 class MyTestCase(LiveServerTestCase):
     def setUp(self):
         chrome_options = Options()
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--disable-dev-shm-usage')
-        self.browser = webdriver.Chrome('/usr/local/bin/chromedriver', chrome_options=chrome_options)
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        self.browser = webdriver.Chrome(
+            "/usr/local/bin/chromedriver", chrome_options=chrome_options
+        )
 
     def tearDown(self):
-        print('we made it')
+        print("we made it")
         self.browser.quit()
 
     def test_smoke(self):
-        self.browser.get('%s%s' % (self.live_server_url, '/'))
+        self.browser.get("%s%s" % (self.live_server_url, "/"))
