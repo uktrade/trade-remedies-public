2a05ce779a17658fb8ba1f90a31b0ef4631da0e3
Christopher Pettinga <chrispettinga@gmail.com>
2022-05-05 13:36:56 +0100

    Selenium tests should run headless

--- trade_remedies_public/login/tests/test_login.py
+++ trade_remedies_public/login/tests/test_login.py
@@ -7,7 +7,7 @@ from django.conf import settings
 class TestLogin(LiveServerTestCase):
     def setUp(self):
         chrome_options = Options()
-        #chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         self.browser = webdriver.Chrome(chrome_options=chrome_options)
