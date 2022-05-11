a9dfaddd8b0a64d685a6379d8ec3fc484a06359a
Johnny <knowyourlines@outlook.com>
2022-05-11 11:11:46 +0100

    Revert "added first functional test"
    This reverts commit c29ffaf5

--- trade_remedies_public/functional_tests/test_me.py
+++ trade_remedies_public/functional_tests/test_me.py
@@ -1,11 +1,9 @@
 from django.test.testcases import LiveServerTestCase
-from django.urls import reverse
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
 
 
-class ForgottenPasswordTestCase(LiveServerTestCase):
+class MyTestCase(LiveServerTestCase):
     def setUp(self):
         chrome_options = Options()
         chrome_options.add_argument("--headless")
@@ -14,10 +12,8 @@ class ForgottenPasswordTestCase(LiveServerTestCase):
         self.browser = webdriver.Chrome(chrome_options=chrome_options)
 
     def tearDown(self):
+        print("we made it")
         self.browser.quit()
 
-    def test_forgotten_password_button_redirects_to_forgot_password_page(self):
-        self.browser.get(f"{self.live_server_url}/")
-        self.browser.find_element(By.ID, "sign_in_button").click()
-        self.browser.find_element(By.ID, "forgot_password_link").click()
-        self.assertIn(reverse("forgot_password"), self.browser.current_url)
+    def test_smoke(self):
+        self.browser.get("%s%s" % (self.live_server_url, "/"))
