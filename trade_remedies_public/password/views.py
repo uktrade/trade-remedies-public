d6ff817bfcc81808147310d4baaa337c1613fa76
Johnny <knowyourlines@outlook.com>
2022-05-24 03:37:03 +0100

    enable password reset request via user primary key

--- trade_remedies_public/password/views.py
+++ trade_remedies_public/password/views.py
@@ -1,16 +1,32 @@
 # Views to handle the forgotten and reset password functionality
+import requests
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views.generic import TemplateView
 
-from trade_remedies_client.exceptions import APIException
 from trade_remedies_client.mixins import TradeRemediesAPIClientMixin
 
 from password.decorators import v2_error_handling
 
+from config.settings.base import API_BASE_URL, HEALTH_CHECK_TOKEN, ENVIRONMENT_KEY
+
 
 class ForgotPasswordRequested(TemplateView, TradeRemediesAPIClientMixin):
     template_name = "password/password_reset_requested.html"
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        user_pk = request.GET.get("user_pk")
+        headers = {
+            "Authorization": f"Token {HEALTH_CHECK_TOKEN}",
+            "X-Origin-Environment": ENVIRONMENT_KEY,
+            "X-User-Agent": "",
+            "X-Forwarded-For": "",
+        }
+        url = f"{API_BASE_URL}/api/v2/accounts/password/pk_request_reset/"
+        if user_pk:
+            requests.get(url, headers=headers, params={"user_pk": user_pk})
+        return self.render_to_response(context)
 
 
 class ForgotPasswordView(TemplateView, TradeRemediesAPIClientMixin):
@@ -27,7 +43,13 @@ class ResetPasswordView(TemplateView, TradeRemediesAPIClientMixin):
     def get(self, request, user_pk, token, *args, **kwargs):
         token_is_valid = self.trusted_client.validate_password_reset(user_pk=user_pk, token=token)
         if not token_is_valid:
-            return render(request, "v2/password/reset_password_expired.html")
+            return render(
+                request,
+                "v2/password/reset_password_expired.html",
+                {
+                    "user_pk": user_pk,
+                },
+            )
         error_message = kwargs.get("error", None)
         return render(
             request,
