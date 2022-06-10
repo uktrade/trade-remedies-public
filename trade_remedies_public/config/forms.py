f083513b53a0d028a6108b91eb07b622ad17ed94
christopherpettinga <chrispettinga@gmail.com>
2022-06-10 15:50:20 +0100

    Implemented Mary's comments on https://uktrade.atlassian.net/browse/TRSV2-205?focusedCommentId=94216

--- trade_remedies_public/config/forms.py
+++ trade_remedies_public/config/forms.py
@@ -32,7 +32,7 @@ class CustomValidationForm(forms.Form, TradeRemediesAPIClientMixin):
                                 request.session["form_errors"][field].append(error_text)
                     if error_summary := validation_error.get("error_summary"):
                         # We don't want to show the same error_summary twice
-                        if error_summary not in request.session["form_errors"]["error_summaries"]:
+                        if error_summary not in [each[1] for each in request.session["form_errors"]["error_summaries"]]:
                             request.session["form_errors"]["error_summaries"].append(
                                 (field, error_summary)
                             )
