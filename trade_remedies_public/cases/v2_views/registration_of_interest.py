0c17d833466554d6f8e4d8aff72771d1b117591c
christopherpettinga <chrispettinga@gmail.com>
2022-06-15 11:31:30 +0100

    Continuing work

--- trade_remedies_public/cases/v2_views/registration_of_interest.py
+++ trade_remedies_public/cases/v2_views/registration_of_interest.py
@@ -1,5 +1,10 @@
 from django.views.generic import TemplateView
+from trade_remedies_client.mixins import TradeRemediesAPIClientMixin
 
 
-class RegistrationOfInterest1(TemplateView):
+class RegistrationOfInterest1(TemplateView, TradeRemediesAPIClientMixin):
     template_name = "v2/registration_of_interest/registration_of_interest_1.html"
+
+    def get_context_data(self, **kwargs):
+        cases = self.client(self.request.user).get_all_cases("registration-of-interest")
+        print("ads")
