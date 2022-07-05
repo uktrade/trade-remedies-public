64d1dbe6767ea2c207a17a29892901948287a6f1
johnny <jonathan.li@digital.trade.gov.uk>
2022-07-05 14:22:03 +0100

    do not use v2 roi step 2 for now

--- trade_remedies_public/cases/views.py
+++ trade_remedies_public/cases/views.py
@@ -541,8 +541,9 @@ class CompanyView(LoginRequiredMixin, GroupRequiredMixin, BasePublicView):
         form_action = self.get_submit_urls("company", case_id=case_id, submission_id=submission_id)
         sub_type_key = self.submission_type_key or "application"
         template_name = f"cases/submissions/{sub_type_key}/company_info.html"
-        if sub_type_key == "interest":
-            return redirect(f"/case/interest/{case_id}/type/")  # noqa: E501
+        # TODO: Uncomment below to enable v2 registration of interest step 2
+        # if sub_type_key == "interest":
+        #     return redirect(f"/case/interest/{case_id}/type/")  # noqa: E501
 
         page = request.GET.get("page") or 1
         return render(
