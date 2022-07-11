b53d22d10a99c5448022e8c5a58eb9c51acaad5a
christopherpettinga <chrispettinga@gmail.com>
2022-07-11 10:24:48 +0100

    Removed call to v2 client login

--- trade_remedies_public/config/settings/test.py
+++ trade_remedies_public/config/settings/test.py
@@ -9,7 +9,7 @@ CACHES = {
     },
 }
 
-# speed up tests
+# speed up tests by using an easier hashing function
 # https://docs.djangoproject.com/en/3.0/topics/testing/overview/#password-hashing
 PASSWORD_HASHERS = [
     "django.contrib.auth.hashers.MD5PasswordHasher",
