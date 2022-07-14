e5e4778d910b0b96045326d2976380656113f268
Christopher Pettinga <chrispettinga@gmail.com>
2022-07-14 14:44:27 +0100

    Feature - TRSV2-238 - V2 API Client (#170)
    * Starting work on the v2 client
    
    * Added trs_v2_api_client as a new submodule
    
    Moved the quicker password hasher to the test.py config as there's no reason it should be in local
    
    * deleted v2 client submodule
    
    * Added mixin to login view
    
    * Added new V2 API client to requirements.
    
    * black checks
    
    * Removed call to v2 client login
    
    * Removed call to v2 client login
    
    * [ci skip] AUTOMATED - update fitness functions
    
    * [ci skip] AUTOMATED - update fitness functions
    
    * [ci skip] AUTOMATED - update fitness functions
    
    Co-authored-by: FITNESS-FUNCTIONS MACHINE USER <fitness-functions-machine-user@digital.trade.gov.uk>

--- trade_remedies_public/config/settings/local.py
+++ trade_remedies_public/config/settings/local.py
@@ -4,12 +4,6 @@ INSTALLED_APPS += [  # noqa
     "behave_django",
 ]
 
-# speed up tests
-# https://docs.djangoproject.com/en/3.0/topics/testing/overview/#password-hashing
-PASSWORD_HASHERS = [
-    "django.contrib.auth.hashers.MD5PasswordHasher",
-]
-
 # SELENIUM
 SELENIUM_BROWSER = "chrome"
 SELENIUM_HUB_HOST = "selenium-hub"
