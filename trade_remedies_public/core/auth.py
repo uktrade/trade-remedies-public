513f02f1ec27b36c575191820b5dad341f6a57c4
Steve Holden <steve.holden@digital.trade.co.uk>
2022-12-07 11:21:52 +0000

    Trivial simplifications, 1

--- trade_remedies_public/core/auth.py
+++ trade_remedies_public/core/auth.py
@@ -1,25 +1,32 @@
+"""
+auth.py: Public client authentication.
+"""
 from django.contrib.auth.models import User
 from trade_remedies_client.client import Client
 
-
+#
+# This code was probably copied,
+# so I have had to edit it twice.
+# Such repetitions are counterproductive.
+#
 class TransientUser(User):
     def has_group(self, groups):
-        if not isinstance(groups, list):
+        if isinstance(groups, str):
             groups = [groups]
         return any([grp in self.groups for grp in groups])
 
 
 class AuthenticationBackend:
     def authenticate(self, request, username=None, password=None, **kwargs):  # noqa
-        response_data = Client().authenticate(username, password)
-        if response_data and response_data.get("token"):
-            request.session["token"] = response_data["token"]
-            request.session["user"] = response_data["user"]
+        rd = Client().authenticate(username, password)
+        if rd and rd.get("token"):
+            request.session["token"] = rd["token"]
+            request.session["user"] = rd["user"]
 
-        user = TransientUser(username=username, token=response_data["token"])
+        user = TransientUser(username=username, token=rd["token"])
         user.is_authenticated = True
         user.backend = self
-        user.groups = response_data["groups"]
+        user.groups = rd["groups"]
         return user
 
     def has_perm(self, perm, **kwargs):
