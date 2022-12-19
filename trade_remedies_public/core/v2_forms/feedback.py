085f48d067b683cad4a5a9cd04ef43f2f614277a
christopherpettinga <chrispettinga@gmail.com>
2022-12-19 16:55:34 +0000

    black

--- trade_remedies_public/core/v2_forms/feedback.py
+++ trade_remedies_public/core/v2_forms/feedback.py
@@ -4,12 +4,13 @@ from config.forms import ValidationForm
 
 
 class FeedbackForm(ValidationForm):
-    rating = forms.MultipleChoiceField(choices=(
-        ("1", "Very dissatisfied"),
-        ("2", "Dissatisfied"),
-        ("3", "Neither satisfied or dissatisfied"),
-        ("4", "Satisfied"),
-        ("5", "Very satisfied"),
-    ), error_messages={
-        "required": "feedback_no_rating"
-    })
+    rating = forms.MultipleChoiceField(
+        choices=(
+            ("1", "Very dissatisfied"),
+            ("2", "Dissatisfied"),
+            ("3", "Neither satisfied or dissatisfied"),
+            ("4", "Satisfied"),
+            ("5", "Very satisfied"),
+        ),
+        error_messages={"required": "feedback_no_rating"},
+    )
