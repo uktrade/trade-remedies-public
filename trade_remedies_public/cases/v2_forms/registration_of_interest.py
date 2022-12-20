c045d3ca8a76630c47a1d5b6f1b4d9668edd0fc8
Yusuf <yusuf.sheikh@digital.trade.gov.uk>
2022-12-20 12:16:15 +0000

    Refactor: Abstract Companies House accessible autocomplete search

--- trade_remedies_public/cases/v2_forms/registration_of_interest.py
+++ trade_remedies_public/cases/v2_forms/registration_of_interest.py
@@ -43,32 +43,27 @@ class YourEmployerForm(BaseYourEmployerForm):
 
 
 class UkEmployerForm(ValidationForm):
-    organisation_name = forms.CharField()
-    companies_house_id = forms.CharField()
-    organisation_post_code = forms.CharField()
-    organisation_address = forms.CharField()
-    # Need a field to match element id in the form html template to add error message
-    company_search_container = forms.CharField(widget=forms.HiddenInput(), required=False)
+    company_data = forms.JSONField(widget=forms.HiddenInput(), required=False)
 
     def clean(self):
         # The user has entered something in the autocomplete box but not selected an option
-        if (
-            self.data.get("input-autocomplete")
-            and not self.cleaned_data.get("organisation_name")
-            and not self.cleaned_data.get("companies_house_id")
-            and not self.cleaned_data.get("organisation_post_code")
-            and not self.cleaned_data.get("organisation_address")
-        ):
-            self.add_error("company_search_container", "companies_house_not_selected")
+        if self.data.get("input-autocomplete") and not self.cleaned_data.get("company_data"):
+            self.add_error("company_data", "companies_house_not_selected")
         # Nothing has been entered by the user
         elif not self.data.get("input-autocomplete"):
-            self.add_error("company_search_container", "companies_house_not_searched")
+            self.add_error("company_data", "companies_house_not_searched")
         else:
+            company = self.cleaned_data["company_data"]
+            company_postcode = company["address"]["postal_code"]
+
+            self.cleaned_data["companies_house_id"] = company["company_number"]
+            self.cleaned_data["organisation_name"] = company["title"]
             self.cleaned_data["organisation_address"] = (
-                self.cleaned_data["organisation_address"]
-                .removesuffix(self.cleaned_data["organisation_post_code"])
+                company["address_snippet"]
+                .removesuffix(company_postcode)
                 .rstrip(", ")
             )
+            self.cleaned_data["organisation_post_code"] = company_postcode
 
             return self.cleaned_data
 
