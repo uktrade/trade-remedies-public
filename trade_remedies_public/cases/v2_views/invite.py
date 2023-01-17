4fe509f67bcb959c2be68b341ccec99565fb5059
Yusuf <yusuf.sheikh@digital.trade.gov.uk>
2023-01-17 18:28:53 +0000

    Refactor: Remove redundant context data

--- trade_remedies_public/cases/v2_views/invite.py
+++ trade_remedies_public/cases/v2_views/invite.py
@@ -203,11 +203,10 @@ class ReviewInvitation(BaseInviteView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        cases = self.client.organisations(
-            self.request.user.contact["organisation"]["id"], fields=["cases", "user_cases"]
+        organisation = self.client.organisations(
+            self.request.user.contact["organisation"]["id"], fields=["user_cases"]
         )
-        context["cases"] = cases
-        context["user_cases"] = cases.user_cases
+        context["organisation"] = organisation
         return context
 
     def post(self, request, *args, **kwargs):
@@ -216,13 +215,6 @@ class ReviewInvitation(BaseInviteView):
 
 
 class InvitationSent(BaseInviteView):
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["cases"] = self.client.organisations(
-            self.request.user.contact["organisation"]["id"], fields=["cases"]
-        )
-        return context
-
     template_name = "v2/invite/sent.html"
 
 
