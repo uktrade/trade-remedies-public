92a16bcee6e8a885885de75d3c8849869dca9c9d
Uka Osim <hsojo91@gmail.com>
2023-01-23 17:25:56 +0000

    write our won nonce string

--- trade_remedies_public/core/templatetags/analytics.py
+++ trade_remedies_public/core/templatetags/analytics.py
@@ -1,3 +1,6 @@
+import os
+import base64
+
 from core.templatetags import register
 from django.utils.safestring import mark_safe
 from django.conf import settings
@@ -15,8 +18,17 @@ def analytics(context, body=False):
 
     output = []
     cookie_policy = context.get("cookie_policy") or {}
-    request = context.get("request")
+
     if cookie_policy.get("accept_gi") == "on":
+        # solution from:
+        # https://github.com/mozilla/django-csp/blob/e209183b0956bfe2cef562bece18a6a526479c80/csp/middleware.py
+        nonce = None
+        request = context.get("request")
+        if request:
+            nonce = getattr(request, "_csp_nonce", None)
+            if not nonce:
+                nonce = base64.b64encode(os.urandom(16)).decode("ascii")
+                setattr(request, "_csp_nonce", nonce)
         if body:
             output = [
                 "<!-- Google Tag Manager (noscript) -->",
@@ -29,7 +41,7 @@ def analytics(context, body=False):
             output = [
                 "<!-- Google Tag Manager -->",
                 "<script nonce='",
-                request.csp_nonce if request else "",
+                nonce or "",
                 "'>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':",
                 "new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],",
                 "j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=",
