bcddd905a7b8f14ad9ab2af40593e9b87fb18fe5
Uka Osim <hsojo91@gmail.com>
2023-01-23 16:06:33 +0000

    bug fix

--- trade_remedies_public/core/templatetags/analytics.py
+++ trade_remedies_public/core/templatetags/analytics.py
@@ -15,7 +15,7 @@ def analytics(context, body=False):
 
     output = []
     cookie_policy = context.get("cookie_policy") or {}
-    request = context["request"]
+    request = context.get("request")
     if cookie_policy.get("accept_gi") == "on":
         if body:
             output = [
@@ -29,7 +29,7 @@ def analytics(context, body=False):
             output = [
                 "<!-- Google Tag Manager -->",
                 "<script nonce='",
-                request.csp_nonce,
+                request.csp_nonce if request else "",
                 "'>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':",
                 "new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],",
                 "j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=",
