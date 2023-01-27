00e7285804fbd40d375729155cab64cdd9b09319
Uka Osim <hsojo91@gmail.com>
2023-01-27 18:35:58 +0000

    wip

--- trade_remedies_public/config/urls.py
+++ trade_remedies_public/config/urls.py
@@ -21,7 +21,7 @@ from django.urls import (
 from cases.views import CasesView
 from cookies import views as cookie_views
 from core import views as core_views
-from core.v2_views import feedback, manage_your_team
+from core.v2_views import feedback, manage_users
 from login import views as login_views
 from password import views as password_views
 from registration.views import views as register_views
@@ -301,9 +301,7 @@ urlpatterns = [
 ]
 
 urlpatterns += [
-    path(
-        "manage_your_team", manage_your_team.ManageYourTeamView.as_view(), name="manage_your_team"
-    ),
+    path("manage-users", manage_users.ManageUsersView.as_view(), name="manage-users"),
     path("collect_feedback", feedback.CollectFeedbackView.as_view(), name="collect_feedback"),
     path(
         "collect_feedback_second_step/<uuid:feedback_id>",
--- trade_remedies_public/core/v2_views/manage_users.py
+++ trade_remedies_public/core/v2_views/manage_users.py
@@ -7,14 +7,12 @@ from config.constants import (
 )
 
 
-class ManageYourTeamView(BasePublicView, TemplateView):
-    template_name = "v2/mange_your_team/main.html"
+class ManageUsersView(BasePublicView, TemplateView):
+    template_name = "v2/manage_users/main.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["organisation"] = self.client().organisations.retrieve(
-            self.request.user.organisation["id"]
-        )
+        context["organisation"] = self.client.organisations(self.request.user.organisation["id"])
         context["group_owner"] = SECURITY_GROUP_ORGANISATION_OWNER
         context["group_regular"] = SECURITY_GROUP_ORGANISATION_USER
         return context
