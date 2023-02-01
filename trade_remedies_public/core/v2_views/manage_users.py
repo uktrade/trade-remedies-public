962b4aea6a6a340550fc74e83aa9e4f4d92f1f4a
Uka Osim <hsojo91@gmail.com>
2023-02-01 01:10:11 +0000

    wip

--- trade_remedies_public/core/v2_views/manage_users.py
+++ trade_remedies_public/core/v2_views/manage_users.py
@@ -1,10 +1,7 @@
 from django.views.generic import TemplateView
 
 from config.base_views import BasePublicView
-from config.constants import (
-    SECURITY_GROUP_ORGANISATION_OWNER,
-    SECURITY_GROUP_ORGANISATION_USER,
-)
+from config.constants import SECURITY_GROUP_ORGANISATION_OWNER, SECURITY_GROUP_THIRD_PARTY_USER
 
 
 class ManageUsersView(BasePublicView, TemplateView):
@@ -13,6 +10,17 @@ class ManageUsersView(BasePublicView, TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["organisation"] = self.client.organisations(self.request.user.organisation["id"])
+        context["pending_invitations"] = self.client.invitations(
+            organisation_id=self.request.user.organisation["id"],
+            approved_at__isnull=True,
+            rejected_at__isnull=True,
+        )
+        context["rejected_invitations"] = self.client.invitations(
+            organisation_id=self.request.user.organisation["id"],
+            approved_at__isnull=True,
+            rejected_at__isnull=False,
+        )
+        context["user"] = self.request.user
         context["group_owner"] = SECURITY_GROUP_ORGANISATION_OWNER
-        context["group_regular"] = SECURITY_GROUP_ORGANISATION_USER
+        context["group_third_party"] = SECURITY_GROUP_THIRD_PARTY_USER
         return context
