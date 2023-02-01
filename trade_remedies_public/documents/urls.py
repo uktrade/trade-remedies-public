c9649d2d7d8586586848841d47417e818b1bcad4
Yusuf <yusuf.sheikh@digital.trade.gov.uk>
2023-02-01 20:23:42 +0000

    Add accessibility improvements for file uploads

--- trade_remedies_public/documents/urls.py
+++ trade_remedies_public/documents/urls.py
@@ -4,4 +4,10 @@ from documents import views
 urlpatterns = [
     path("document/", views.DocumentView.as_view(), name="document"),
     path("document/<uuid:document_id>", views.DocumentView.as_view(), name="download_document"),
+    path("document/without-js", views.DocumentWithoutJsView.as_view(), name="document_without_js"),
+    path(
+        "document/without-js/remove/<uuid:document_id>",
+        views.DocumentWithoutJsView.as_view(),
+        name="remove_document_without_js",
+    ),
 ]
--- trade_remedies_public/documents/views.py
+++ trade_remedies_public/documents/views.py
@@ -25,10 +25,14 @@ class DocumentView(View, APIClientMixin):
 
     @catch_form_errors()
     def post(self, request, *args, **kwargs):
+        print(request.POST)
+        print(request.FILES)
         uploaded_files = []
         for file in request.FILES.getlist("files"):
+            print(file)
+            print("INSIDE LOOP")
             form = DocumentForm(data={"file": file}, user=request.user)
-            # Checking the file is valid (size, virus, extension)
+            # Checking the file is valid (size, extension)
             if form.is_valid():
                 # Sending it to the API for storage
                 new_document_upload = self.client.documents(
@@ -60,9 +64,92 @@ class DocumentView(View, APIClientMixin):
                 return JsonResponse(data={"errors": form.errors}, status=400)
 
     def delete(self, request, *args, **kwargs):
+        print(request.GET["document_to_delete"])
         self.client.documents(request.GET["document_to_delete"]).delete()
         return HttpResponse(status=204)
 
     def get(self, request, *args, **kwargs):
         document = self.client.documents(self.kwargs["document_id"])
         return redirect(document["file"])
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+class DocumentWithoutJsView(View, APIClientMixin):
+    def get(self, request, *args, **kwargs):
+        print(request.GET)
+        print(request.POST)
+        print(self.kwargs["document_id"])
+        self.client.documents(self.kwargs["document_id"]).delete()
+        HttpResponse(status=204)
+        return redirect(request.META["HTTP_REFERER"])
+
+    @catch_form_errors()
+    def post(self, request, *args, **kwargs):
+        print("files", request.FILES)
+        print("post", request.POST)
+
+        if "non_confidential_submit" in request.POST:
+            for file in request.FILES.getlist("non_confidential_file"):
+                print(file)
+                print("file name", file.name)
+                print("file size", file.file_size)
+                print("original_name", file.original_name)
+
+                form = DocumentForm(data={"file": file}, user=request.user)
+                print(form)
+                # file.delete()
+                # Checking the file is valid (size, virus, extension)
+                if form.is_valid():
+                    print("VALID")
+                    # Sending it to the API for storage
+                    self.client.documents(
+                        {
+                            "type": "non_confidential",
+                            "stored_name": file.name,
+                            "original_name": file.original_name,
+                            "file_size": file.file_size,
+                            "submission_id": request.POST["non_confidential_submission_id"],
+                            "parent": request.POST.get("parent", None),
+                            "submission_document_type": request.POST.get(
+                                "submission_document_type", None
+                            ),
+                            "replace_document_id": request.POST.get("replace_document_id", None),
+                        }
+                    )
+                else:
+                    print("non", form.errors)
+                    form.assign_errors_to_request(request)
+                    request.session["form_errors"]["file_type"] = "non_confidential"
+        if "confidential_submit" in request.POST:
+            for file in request.FILES.getlist("confidential_file"):
+                print(file)
+                print("file name", file.name)
+                print("file size", file.file_size)
+                print("original_name", file.original_name)
+
+                form = DocumentForm(data={"file": file}, user=request.user)
+                print(form)
+                # file.delete()
+                # Checking the file is valid (size, virus, extension)
+                if form.is_valid():
+                    print("VALID")
+                    # Sending it to the API for storage
+                    self.client.documents(
+                        {
+                            "type": "confidential",
+                            "stored_name": file.name,
+                            "original_name": file.original_name,
+                            "file_size": file.file_size,
+                            "submission_id": request.POST["confidential_submission_id"],
+                            "parent": request.POST.get("parent", None),
+                            "submission_document_type": request.POST.get(
+                                "submission_document_type", None
+                            ),
+                            "replace_document_id": request.POST.get("replace_document_id", None),
+                        }
+                    )
+                else:
+                    form.assign_errors_to_request(request)
+                    request.session["form_errors"]["file_type"] = "confidential"
+
+        return redirect(request.META["HTTP_REFERER"])
