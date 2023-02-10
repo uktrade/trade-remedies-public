d29fd1c37524a503b79cad606e555ddc4ea4d94e
Nawaz <nawaz.ahmed@digital.trade.gov.uk>
2023-02-10 15:38:22 +0000

    flake8 change

--- trade_remedies_public/cases/v2_views/accept_representative_invitation.py
+++ trade_remedies_public/cases/v2_views/accept_representative_invitation.py
@@ -68,8 +68,8 @@ class TwoFactorChoice(AcceptOrganisationTwoFactorChoice):
             self.client.users(self.invitation.invited_user.id).update({"is_active": True})
 
             if self.invitation.invitation_type == 2:
-                # Now let's add the correct groups to the user, so they can log in and the rest of the
-                # invitation can be processed
+                # Now let's add the correct groups to the user, so they can log in and the
+                # rest of the invitation can be processed
                 self.client.users(self.invitation.invited_user.id).add_group(
                     SECURITY_GROUP_ORGANISATION_OWNER
                 )
