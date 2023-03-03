a7884609813c779264749899f87fc83460fc7d9f
Yusuf <yusuf.sheikh@digital.trade.gov.uk>
2023-03-03 12:28:53 +0000

    Add descriptive label for "show/hide" password button

--- trade_remedies_public/templates/static/v2/js/show_password.js
+++ trade_remedies_public/templates/static/v2/js/show_password.js
@@ -1,13 +1,15 @@
 $('#show_password').click(function () {
+  'use strict';
+
   if ($(this).attr('aria-checked') === 'false') {
-    $(this).attr('aria-checked', 'true')
-    $('#password').attr('type', 'text')
-    $(this).find('span').text('Hide')
-    $("#password-text").html("Password shown.")
+    $(this).attr('aria-checked', 'true');
+    $('#password').attr('type', 'text');
+    $(this).find('span').html('Hide <span class="govuk-visually-hidden">password</span>');
+    $("#password-text").html("Password shown.");
   } else {
-    $(this).attr('aria-checked', 'false')
-    $('#password').attr('type', 'password')
-    $(this).find('span').text('Show')
-    $("#password-text").html("Password hidden.")
+    $(this).attr('aria-checked', 'false');
+    $('#password').attr('type', 'password');
+    $(this).find('span').html('Show <span class="govuk-visually-hidden">password</span>');
+    $("#password-text").html("Password hidden.");
   }
-})
+});
