234e555d5141eda2ba774f77b2a7a0d0c62e840e
christopherpettinga <chrispettinga@gmail.com>
2023-05-23 15:57:32 +0100

    Abstracted exceptions that log to sentry into their own base class so others can inherit. Also set tag so Sentry can trigger alert rule so we're always AWARE

--- trade_remedies_public/core/exceptions.py
+++ trade_remedies_public/core/exceptions.py
@@ -1,11 +1,16 @@
 from django.core.exceptions import PermissionDenied
+from sentry_sdk import capture_exception, configure_scope
 
 
-class SentryPermissionDenied(PermissionDenied):
-    """Raises a 503 PermissionDenied error but also logs to sentry"""
+class LogToSentryError(Exception):
+    """Base class for suspicious operations which should be logged to sentry and alert the team"""
 
     def __init__(self, message=None, *args, **kwargs):
         super().__init__(message, *args, **kwargs)
-        from sentry_sdk import capture_exception
+        with configure_scope() as scope:
+            scope.set_tag("alert_team", True)
+            capture_exception(self)
 
-        capture_exception(self)
+
+class SentryPermissionDenied(PermissionDenied, LogToSentryError):
+    """Raises a 403 PermissionDenied error but also logs to sentry"""
