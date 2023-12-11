59c57bbb179c0cef0ef5fc4f900d336df91c3569
christopherpettinga <chrispettinga@gmail.com>
2023-12-11 14:51:09 +0000

    fixing URLS

--- trade_remedies_public/config/urls.py
+++ trade_remedies_public/config/urls.py
@@ -17,9 +17,9 @@ from django.urls import (
     include,
     path,
 )
-
 from django.views.generic import TemplateView
 
+from cases.v2_views import active_investigations
 from cases.views import CasesView
 from cookies import views as cookie_views
 from core import views as core_views
@@ -27,7 +27,6 @@ from core.v2_views import feedback, manage_users
 from login import views as login_views
 from password import views as password_views
 from registration import views as register_views
-from cases.v2_views import active_investigations
 
 # todo - config/urls.py should not contain anything, put these URLs in their relevant apps
 urlpatterns = [
@@ -226,141 +225,6 @@ urlpatterns = [
     path("stub/", core_views.StubView.as_view(), name="stub"),
     path(
         "companieshouse/search/", core_views.CompaniesHouseSearch.as_view(), name="companieshouse"
-    ),
-    path(
-        "register/confirm_existing_org",
-        register_views.V2RegistrationViewConfirmExistingOrganisation.as_view(),
-        name="v2_confirm_existing_org",
-    ),
-    path(
-        "register/how_to_get_account",
-        TemplateView.as_view(template_name="v2/registration/how_to_get_account.html"),
-        name="v2_how_to_get_account",
-    ),
-    path(
-        "register/start", register_views.V2RegistrationViewStart.as_view(), name="v2_register_start"
-    ),
-    path(
-        "register/set_password",
-        register_views.V2RegistrationViewSetPassword.as_view(),
-        name="v2_register_set_password",
-    ),
-    path(
-        "register/2fa_choice",
-        register_views.V2RegistrationView2FAChoice.as_view(),
-        name="v2_register_2fa_choice",
-    ),
-    path(
-        "register/your_employer",
-        register_views.V2RegistrationViewYourEmployer.as_view(),
-        name="v2_register_your_employer",
-    ),
-    path(
-        "register/your_uk_employer",
-        register_views.V2RegistrationViewUkEmployer.as_view(),
-        name="v2_register_your_uk_employer",
-    ),
-    path(
-        "register/your_non_uk_employer",
-        register_views.V2RegistrationViewNonUkEmployer.as_view(),
-        name="v2_register_your_non_uk_employer",
-    ),
-    path(
-        "register/organisation_further_details",
-        register_views.V2RegistrationViewOrganisationFurtherDetails.as_view(),
-        name="v2_register_organisation_further_details",
-    ),
-    path(
-        "register/verify_email/<uuid:user_pk>",
-        register_views.RequestEmailVerifyCode.as_view(),
-        name="request_email_verify_code",
-    ),
-    path(
-        "register/verify_email/<uuid:user_pk>/<str:email_verify_code>",
-        register_views.VerifyEmailVerifyCode.as_view(),
-        name="email_verify_code",
-    ),
-    path("documents/", include("documents.urls")),
-]
-
-urlpatterns += [
-    path("collect_feedback", feedback.CollectFeedbackView.as_view(), name="collect_feedback"),
-    path(
-        "collect_feedback_second_step/<uuid:feedback_id>",
-        feedback.CollectFeedbackView.as_view(),
-        name="collect_feedback_second_step",
-    ),
-    path("collect_rating", feedback.CollectRatingView.as_view(), name="collect_rating"),
-    path(
-        "feedback_sent/<uuid:feedback_id>",
-        feedback.FeedbackSentView.as_view(),
-        name="feedback_sent",
-    ),
-]
-
-# Manage team urls
-urlpatterns += [
-    path("manage_users_home", manage_users.ManageUsersView.as_view(), name="manage_users_home"),
-    path(
-        "view_user/<uuid:organisation_user_id>/", manage_users.ViewUser.as_view(), name="view_user"
-    ),
-    path(
-        "view_user/<uuid:user_id>/<uuid:organisation_id>/",
-        manage_users.ViewUser.as_view(),
-        name="view_user_organisation_user",
-    ),
-    path(
-        "edit_user/<uuid:organisation_user_id>/", manage_users.EditUser.as_view(), name="edit_user"
-    ),
-    path(
-        "edit_user/<uuid:organisation_user_id>/change_permissions",
-        manage_users.ChangeOrganisationUserPermissionsView.as_view(),
-        name="change_organisation_user_permissions",
-    ),
-    path(
-        "edit_user/<uuid:organisation_user_id>/change_user_active",
-        manage_users.ChangeUserActiveView.as_view(),
-        name="change_organisation_user_active",
-    ),
-    path(
-        "edit_user/<uuid:organisation_user_id>/change_case_role/<uuid:user_case_id>/",
-        manage_users.ChangeCaseRoleView.as_view(),
-        name="change_case_role",
-    ),
-    path(
-        "edit_user/<uuid:organisation_user_id>/remove_from_case/<uuid:user_case_id>/",
-        manage_users.RemoveFromCaseView.as_view(),
-        name="remove_from_case",
-    ),
-    path(
-        "edit_user/<uuid:organisation_user_id>/assign_to_case/",
-        manage_users.AssignToCaseView.as_view(),
-        name="assign_to_case",
-    ),
-]
-
-# public active investigations page
-urlpatterns += [
-    path(
-        "public/case/<str:case_number>/submission/<uuid:submission_id>"
-        "/document/<uuid:document_id>/",
-        core_views.PublicDownloadView.as_view(),
-        name="legacy_public_case_submission_document",
-    ),
-    path(
-        "public/cases/",
-        active_investigations.ActiveInvestigationsView.as_view(),
-        name="public_cases",
-    ),
-    path(
-        "public/case/<str:case_number>/",
-        active_investigations.SingleCaseView.as_view(),
-        name="public_case",
-    ),
-    path(
-        "public/case/<str:case_number>/submission/<uuid:submission_id>/",
-        active_investigations.SingleSubmissionView.as_view(),
-        name="public_submission",
     ),
     path(
         "register/confirm_existing_org",
