3f1872edfa133d93f057874c8760c66963a77f05
Christopher Pettinga <chris.pettinga@digital.trade.gov.uk>
2024-11-18 10:49:03 +0000

    fixing basic_auth_user

--- trade_remedies_public/config/cf_env.py
+++ trade_remedies_public/config/cf_env.py
@@ -20,7 +20,7 @@ class CloudFoundrySettings(BaseSettings):
     ALLOWED_HOSTS: str = ""
     ORGANISATION_NAME: str = "Organisation name placeholder"
     ORGANISATION_INITIALISM: str = "Organisation initialism placeholder"
-    BASIC_AUTH_USER: bool = False
+    BASIC_AUTH_USER: dict = {}
     VCAP_SERVICES: Optional[VCAPServices] = {}
     REDIS_DATABASE_NUMBER: int = 2
     REDIS_BASE_URL: str = "redis://redis:6379"
