53ef522eaad140710c61c855446d81c6f79d9c7f
Emile Swarts <emile.swarts@digital.trade.gov.uk>
2025-02-11 15:08:33 +0000

    Don't restrict hosts, this isn't required on DBT Platform

--- trade_remedies_public/config/settings/base.py
+++ trade_remedies_public/config/settings/base.py
@@ -47,7 +47,7 @@ SECRET_KEY = env.DJANGO_SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!!!
 DEBUG = env.DEBUG
 
-ALLOWED_HOSTS = env.get_allowed_hosts()
+ALLOWED_HOSTS = ["*"]
 
 ORGANISATION_NAME = env.ORGANISATION_NAME
 
