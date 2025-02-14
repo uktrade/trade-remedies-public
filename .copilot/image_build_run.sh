f2606c1f7a7f33fe838aef0af7056234e6793b1b
Jahir <jahir.uddin@digital.trade.gov.uk>
2025-02-14 14:47:37 +0000

    added missing env variable to complete build process

--- .copilot/image_build_run.sh
+++ .copilot/image_build_run.sh
@@ -2,5 +2,9 @@
 
 # Exit early if something goes wrong
 set -e
+export API_BASE_URL=http://api:8000
+export DJANGO_SECRET_KEY="example"
+export CASE_WORKER_ENVIRONMENT_KEY="example"
+export DATABASE_URL=psql://postgres:postgres@localhost:5432/trade_remedies
 
 # Add commands below to run inside the container after all the other buildpacks have been applied
