ed9ba85ef9e2ca1e8b1ecba44557c733ec305b13
Jahir <jahir.uddin@digital.trade.gov.uk>
2025-02-20 15:19:55 +0000

    exported django module setting for image build

--- .copilot/image_build_run.sh
+++ .copilot/image_build_run.sh
@@ -6,7 +6,9 @@ export API_BASE_URL=http://api:8000
 export DJANGO_SECRET_KEY="example"
 export CASE_WORKER_ENVIRONMENT_KEY="example"
 export DATABASE_URL=psql://postgres:postgres@localhost:5432/trade_remedies
+export DJANGO_SETTINGS_MODULE="config.settings.staging"
 
 # Add commands below to run inside the container after all the other buildpacks have been applied
 cd trade_remedies_public
-python manage.py collectstatic --noinput --clear
+python manage.py compilescss
+python manage.py collectstatic --noinput
